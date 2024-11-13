72b617fb48c48d1e2517377338d66e2f3c88bd6f
Jasurbek <163736163+Akhmedov76@users.noreply.github.com>
2024-11-13 10:09:11 +0500

    Update urls.py

--- Config/urls.py
+++ Config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='auth'))
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
